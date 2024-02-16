6d882fc769b3a298221b3192bd452ab957c87d1d
ykmsd <yuka1110m@gmail.com>
2024-02-16 10:40:27 +0100

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+// run `node index.js` in the terminal
+const url = 'http://localhost:4000/posts';
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    console.log({ json });
+  })
+  .catch(console.error);
